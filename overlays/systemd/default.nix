b55d37f63368afebae0869eed47159b6c99c903f
ProggerX <x@proggers.ru>
2026-04-01 13:03:13 +0300

    rename systemd overlayed package to not override dependencies

--- overlays/systemd/default.nix
+++ overlays/systemd/default.nix
@@ -1,14 +1,11 @@
 {
   flake.overlays.systemd = finalPkgs: prevPkgs: {
-    systemd = prevPkgs.systemd.overrideAttrs (
+    suwustemd = prevPkgs.systemd.overrideAttrs (
       finalAttrs: prevAttrs: {
         patches = (prevAttrs.patches or [ ]) ++ [
           ./nixowos_support.patch
         ];
       }
     );
-    nix = prevPkgs.nix.overrideAttrs (old: {
-      doCheck = false;
-    });
   };
 }
